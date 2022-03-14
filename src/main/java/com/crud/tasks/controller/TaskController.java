97389eb4aaf14b1ab63c53fad9c16cccc96ebc7e
Aleksandra <ola.odzierzynska@gmail.com>
2022-03-14 22:29:07 +0100

    19.2. Tworzenie połączenia z bazą danych z poziomu aplikacji

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,27 +1,38 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/task")
+@RequiredArgsConstructor
 public class TaskController {
+
+    @Autowired
+    private DbService service;
+    @Autowired
+    private TaskMapper taskMapper;
+
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
-    public List<TaskDto> getTasks() { return new ArrayList<>(); }
+    public List<TaskDto> getTasks() {
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
+    }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L, "test title", "test_content");
+        return taskMapper.mapToTaskDto(service.getTask(taskId));
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
     public void deleteTask(Long taskId) {
-
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "taskDto")
---
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -0,0 +1,28 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class DbService {
+
+    @Autowired
+    private final TaskRepository repository;
+
+    public List<Task> getAllTasks() {
+        return repository.findAll();
+    }
+
+    public Task getTask(Long id) {
+        return (Task) repository.findAllById(Collections.singleton(id));
+    }
+
+}
