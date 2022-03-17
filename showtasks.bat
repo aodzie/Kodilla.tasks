2f285e202d9787e2766201ac67b716ce9e83ddc5
Aleksandra <ola.odzierzynska@gmail.com>
2022-03-17 15:15:40 +0100

    21.4. Stworzenie skryptu shellowego

---
+++ showtasks.bat
@@ -0,0 +1,11 @@
+call runcrud.bat
+if "%ERRORLEVEL%" == "0" goto stoptomcat
+echo Cannot run file
+
+
+:stoptomcat
+call %CATALINA_HOME%\bin\shutdown.bat
+
+
+:openbrowser
+start http://localhost:8080/crud/v1/tasks/getTasks
