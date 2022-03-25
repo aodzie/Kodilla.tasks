a670d0beaa62c2b57d4180c3db197c9b28b4dd11
Aleksandra <ola.odzierzynska@gmail.com>
2022-03-25 14:49:14 +0100

    23.1. Stworzenie widoku pozwalającego na eksport zadań

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -21,30 +21,30 @@ public class TaskController {
     private final DbService service;
     private final TaskMapper taskMapper;
 
-    @GetMapping(value = "getTasks")
+    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public ResponseEntity<List<TaskDto>> getTasks() {
         List<Task> tasks = service.getAllTasks();
         return ResponseEntity.ok(taskMapper.mapToTaskDtoList(tasks));
     }
 
-    @GetMapping(value = "{taskId}")
+    @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public ResponseEntity<TaskDto> getTask(@PathVariable Long taskId) throws TaskNotFoundException{
         return ResponseEntity.ok(taskMapper.mapToTaskDto(service.getTask(taskId)));
     }
 
-    @DeleteMapping(value = "{taskId}")
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
     public ResponseEntity<Void> deleteTask(@PathVariable Long taskId){
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping(value = "createTask")
+    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
     public ResponseEntity<TaskDto> updateTask(@RequestBody TaskDto taskDto) {
         Task task = taskMapper.mapToTask(taskDto);
         Task savedTask = service.saveTask(task);
         return ResponseEntity.ok(taskMapper.mapToTaskDto(savedTask));
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(method = RequestMethod.POST, value = "createTask")
     public ResponseEntity<Void> createTask(@RequestBody TaskDto taskDto) {
         Task task = taskMapper.mapToTask(taskDto);
         service.saveTask(task);
