39f368e9052900b3a0f268f20b310b7ea7af2d8c
Aleksandra <ola.odzierzynska@gmail.com>
2022-03-26 17:59:15 +0100

    23.2. Instalacja Swaggera, czyli dokumentacja dla API

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -22,7 +22,8 @@ public class CoreConfiguration {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                //.apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
